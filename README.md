f3c4cd103bb8a219609c90e321cdbfe42f53050c
Nestor Alfaro <nalfarocs@gmail.com>
2024-05-06 10:40:58 -0500

    c# imports xlsx into sqlite

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Setting up a workflow for scenario two of the provided case study.
 
 ## Available scripts
+Create a python environment with:
+```sh
+python3 -m venv py_env
+source py_env/bin/activate
+```
 ```sh
 airflow webserver -p 8080
 ```
--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -18,16 +18,17 @@ class Program
                 // Get the dimensions of the worksheet
                 int rowCount = worksheet.Dimension.Rows;
                 int columnCount = worksheet.Dimension.Columns;
+                string tableName = $"{worksheet.Name}_{Path.GetFileNameWithoutExtension(sourceXlsx.Name)}";
 
                 string createTableQuery = @$"
-                  CREATE TABLE IF NOT EXISTS {Path.GetFileNameWithoutExtension(sourceXlsx.Name)}
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     sheet TEXT,
+                  CREATE TABLE IF NOT EXISTS {tableName}
+                    (id INTEGER PRIMARY KEY AUTOINCREMENT
+                    , sheet TEXT
                 ";
                 createTableQuery += $", {string.Join(", ", worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column].Select(c => $"\"{c.Text}\" TEXT\n").ToList())}";
                 createTableQuery += ");";
 
-                string insertQuery = $"INSERT INTO {Path.GetFileNameWithoutExtension(sourceXlsx.Name)} ("
+                string insertQuery = $"INSERT INTO {tableName} ("
                                     + string.Join(", ", worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column].Select(c => "\""+c.Text+"\"").ToList())
                                     + ")\nVALUES\n";
                 List<string> rowList = new List<string>();
@@ -46,49 +47,42 @@ class Program
                 insertQuery += $"{string.Join(",", rowList)};";
                 sheetQueryList.Add((createTableQuery, insertQuery));
             }
-
-
-
-
-          // foreach (var s in package.Workbook.Worksheets) {
-          //   string createTableQuery = @$"
-          //     CREATE TABLE IF NOT EXISTS {Path.GetFileNameWithoutExtension(sourceXlsx.Name)}
-          //       (id INTEGER PRIMARY KEY AUTOINCREMENT,
-          //        sheet TEXT,
-          //   ";
-          //   string insertQuery = $"INSERT INTO {Path.GetFileNameWithoutExtension(sourceXlsx.Name)} (";
-          //   // Console.WriteLine($"Columns from sheet {s.Name}:");
-          //   createTableQuery += $", {string.Join(", ", s.Cells[1, 1, 1, s.Dimension.End.Column].Select(c => $"{c.Text} TEXT\n").ToList())}";
-          //   createTableQuery += ");";
-          //   insertQuery += string.Join(", ", s.Cells[1, 1, 1, s.Dimension.End.Column].Select(c => $"[{c.Text}]").ToList()) + ")";
-          //   sheetQueryList.Add((createTableQuery, insertQuery));
-          //   // foreach (var firstRowCell in s.Cells[1, 1, 1, s.Dimension.End.Column]) {
-          //   //   Console.WriteLine(firstRowCell.Text);
-          //   // }
-          //   // createTableQuery += $"{s.Dimension.Columns}";
-          //   // createTableQuery += $"{firstRowCell.Text} TEXT";
-          // }
         }
-        foreach ((string createSql, string insertSql) q in sheetQueryList) {
-          Console.WriteLine(q.createSql);
-          Console.WriteLine(q.insertSql);
-        }
+        // foreach ((string createSql, string insertSql) q in sheetQueryList) {
+        //   Console.WriteLine(q.createSql);
+        //   Console.WriteLine(q.insertSql);
+        // }
 
         string connectionString = $"Data Source={Path.Join(rootDir, "airflow.db")}";
         string testQuery = "SELECT * FROM dag;";
         try {
-          using (SqliteConnection conn = new SqliteConnection(connectionString)) {
-            conn.Open();
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = testQuery;
-            using (var reader = cmd.ExecuteReader()) {
-              while (reader.Read()) {
-                var name = reader.GetString(0);
-                // Console.WriteLine($"Hello, {name}!");
+          foreach ((string createSql, string insertSql) in sheetQueryList) {
+            // Console.WriteLine(q.createSql);
+            // Console.WriteLine(q.insertSql);
+
+            using(SqliteConnection conn = new SqliteConnection(connectionString)) {
+              conn.Open();
+              using (SqliteCommand cmd = new SqliteCommand(createSql, conn)) {
+                Console.WriteLine($"Running:\n{createSql}");
+                int rowsAffected = cmd.ExecuteNonQuery();
+              }
+              using (SqliteCommand cmd = new SqliteCommand(insertSql, conn)) {
+                Console.WriteLine($"Running:\n{insertSql}");
+                int rowsAffected = cmd.ExecuteNonQuery();
               }
             }
           }
-            
+          // using (SqliteConnection conn = new SqliteConnection(connectionString)) {
+          //   conn.Open();
+          //   var cmd = conn.CreateCommand();
+          //   cmd.CommandText = testQuery;
+          //   using (var reader = cmd.ExecuteReader()) {
+          //     while (reader.Read()) {
+          //       var name = reader.GetString(0);
+          //       // Console.WriteLine($"Hello, {name}!");
+          //     }
+          //   }
+          // }
         }
         catch (Exception ex) {
           Console.WriteLine($"ERROR: {ex.Message}");
